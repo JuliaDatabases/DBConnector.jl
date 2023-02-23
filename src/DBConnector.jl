cc5155947adcb790705bbd173d3c2632430696be
TheCedarPrince <jacobszelko@gmail.com>
2023-02-22 20:53:52 -0500

    Exports just DBConnection

--- src/DBConnector.jl
+++ src/DBConnector.jl
@@ -1,18 +1,41 @@
 module DBConnector
-export hello, domath
 
-"""
-    hello(who::String)
+using DBInterface
+using LibPQ
+using SQLite
+using ODBC
+using JDBC
+using MySQL
 
-Return "Hello, `who`".
-"""
-hello(who::String) = "Hello, $who"
+include("postgresql.jl")
+include("sqlite.jl") 
+include("mysql.jl")
+include("odbc.jl")
+include("jdbc.jl")
 
-"""
-    domath(x::Number)
+function DBConnection(dbms; kws...)
 
-Return `x + 5`.
-"""
-domath(x::Number) = x + 5
+    if lowercase(dbms) == "postgresql"
+        return _dbconnect(LibPQ.Connection; kws...)
+    elseif lowercase(dbms) == "sqlite"
+        return _dbconnect(SQLite.DB; kws...)
+    elseif lowercase(dbms) == "mysql"
+        return _dbconnect(MySQL.Connection; kws...)
+    end
+
+end
+
+function DBConnection(;driver_path, connection_string, connectivity, kws...)
+
+    if lowercase(connectivity) == "odbc"
+        _odbcconnect(connection_string, ; kws...)
+    elseif lowercase(connectivity) == "jdbc"
+        _jdbcconnect(driver_path, connection_string, ; kws...)
+    else 
+        println("Connection not made; please specify if this is an JDBC or ODBC connection")
+    end
+end
+
+export DBConnection
 
 end
