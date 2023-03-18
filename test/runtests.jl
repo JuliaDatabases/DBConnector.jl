8c812300686ceccfd05a24651cf5abf2dbccd488
Farreeda <fareedaabdelazeez@gmail.com>
2023-03-18 08:20:19 +0200

    updating PR
    removed .vscode folder
    added

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,8 @@ using Test, Example
 @test domath(2.0) ≈ 7.0
 
 @testset "_dbconnect function for SQLite" begin
-    conn= _dbconnect(SQLite.DB, "path_to_database.db")
+
+    conn= _dbconnect(SQLite.DB, "DBConnector.jl/test/data/database.db")
     @test @isdefined conn
+
 end
