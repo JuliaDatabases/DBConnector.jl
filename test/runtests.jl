5629e7e69b70c733919c32b064ada22154b28229
Farreeda <fareedaabdelazeez@gmail.com>
2023-03-19 10:37:13 +0200

    Edit function and test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using Test, Example
+using MySQL,SQLite, MySQL.API
 
 @test hello("Julia") == "Hello, Julia"
 @test domath(2.0) ≈ 7.0
@@ -9,3 +10,13 @@ using Test, Example
     @test @isdefined conn
 
 end
+ 
+@testset "_dbconnect function for MySQL" begin
+    
+    conn = _dbconnect(MySQL.Connection, "localhost", "debian-sys-maint","HGGsOLypO2LVqq1v", db="mydatabase", port=3306, unix_socket="/var/run/mysqld/mysqld.sock")
+    @test typeof(conn) == MySQL.Connection
+    @test isopen(conn)
+    close(conn)
+
+end
+ 
