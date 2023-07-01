48a66d9205a5471dc5ae1c2bed9daa8ea17cb7fd
Farreeda <fareedaabdelazeez@gmail.com>
2023-07-01 21:21:10 +0300

    updating github secrets

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,7 +13,7 @@ end
  
 @testset "_dbconnect function for MySQL" begin
     
-    conn = _dbconnect(MySQL.Connection, ENV[mysqlhost], ENV[mysqluser], ENV[mysqlpassword], db="MySQL", port=ENV[mysqlport])
+    conn = _dbconnect(MySQL.Connection, ENV['mysqlhost'], ENV['mysqluser'], ENV['mysqlpassword'], db="MySQL", port=ENV['mysqlport'])
 
     @test typeof(conn) == MySQL.Connection
     @test isopen(conn)
@@ -23,7 +23,7 @@ end
 
 @testset "_dbconnect function for LibPQ" begin
 
-    conn= _dbconnect(LibPQ.Connection,  ENV[postgreshost], ENV[postgresuser], ENV[postgrespassword], db = "mimic", port=ENV[postgresport])
+    conn= _dbconnect(LibPQ.Connection,  ENV['postgreshost'], ENV['postgresuser'], ENV['postgrespassword'], db = "mimic", port=ENV['postgresport'])
     @test @isdefined conn
 
 end
