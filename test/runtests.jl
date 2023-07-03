fa480765eb160351c8ec739d6744c6a50e981555
Farreeda <fareedaabdelazeez@gmail.com>
2023-07-03 03:15:24 +0300

    updating the path

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 
 @testset "_dbconnect function for SQLite" begin
 
-    conn= _dbconnect(SQLite.DB, "DBConnector.jl/test/data/database.db")
+    conn= _dbconnect(SQLite.DB, "DBConnector/DBConnector.jl/test/data/SQLite.db")
     @test @isdefined conn
 
 end
