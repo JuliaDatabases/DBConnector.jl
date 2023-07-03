2760adc5a20e275ff6c64308764f0bb4c0b68924
Farreeda <fareedaabdelazeez@gmail.com>
2023-07-03 03:35:45 +0300

    Added quering

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,15 @@
-using Test
+using Test, DataFrames
 
 
 @testset "_dbconnect function for SQLite" begin
 
     conn= _dbconnect(SQLite.DB, "DBConnector/DBConnector.jl/test/data/SQLite.db")
     @test @isdefined conn
+    DBInterface.execute(conn, "CREATE TABLE PERSON (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)")
+    DBInterface.execute(conn, "INSERT INTO PERSON (name, age) VALUES ('John Doe', 30), ('Jane Smith', 25), ('Bob Johnson', 40)")
+    output = DBInterface.execute(conn, "SELECT age FROM PERSON WHERE name = 'John Doe'") |> DataFrame
+    out = output[1,1]
+    expected_output = 30;
+    @test out == expected_output
 
 end
