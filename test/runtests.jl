967e798b6afb5f61c5f302d74d1fe76d97806388
Farreeda <fareedaabdelazeez@gmail.com>
2023-07-03 04:39:42 +0300

    .

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,13 +5,9 @@ include("/DBConnector.jl/src/sqlite.jl")
 
     conn= _dbconnect(SQLite.DB, "DBConnector.jl/test/data/sqlite.db")
     @test @isdefined conn
-    DBInterface.execute(conn, "CREATE TABLE PERSON (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)")
-    DBInterface.execute(conn, "INSERT INTO PERSON (name, age) VALUES ('John Doe', 30), ('Jane Smith', 25), ('Bob Johnson', 40)")
     output = DBInterface.execute(conn, "SELECT age FROM PERSON WHERE name = 'John Doe'") |> DataFrame
     out = output[1,1]
     expected_output = 30;
     @test out == expected_output
-    db_path = "DBConnector.jl/test/data/sqlite.db"
-    rm(db_path)
 
 end
