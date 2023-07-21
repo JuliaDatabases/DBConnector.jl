aadd240e639a20c8d32b471b31050bbca757fee2
TheCedarPrince <jacobszelko@gmail.com>
2023-07-21 15:02:54 -0400

    Updated test suite to work properly

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,12 @@
 using Test, DataFrames, SQLite
 using MySQL,SQLite, MySQL.API, LibPQ
-
-
-include("../src/sqlite.jl")
-include("../src/mysql.jl")
-include("../src/postgresql.jl")
+using DBConnector
 
 @testset "_dbconnect function for SQLite" begin
 
-    conn= _dbconnect(SQLite.DB, "../test/data/sqlite.db")
+    conn= DBConnector._dbconnect(SQLite.DB, "../test/data/sqlite.db")
     @test @isdefined conn
-    output = DBInterface.execute(conn, "SELECT age FROM PERSON WHERE name = 'John Doe'") |> DataFrame
+    output = DBInterface.execute(conn, "SELECT age FROM PERSON WHERE name = 'John Doe';") |> DataFrame
     out = output[1,1]
     expected_output = 30;
     @test out == expected_output
@@ -19,7 +15,7 @@ end
 
 @testset "_dbconnect function for MySQL" begin
     
-    conn = _dbconnect(MySQL.Connection, ENV['MYSQL_HOST'], ENV['MYSQL_USER'], ENV['MYSQL_PASSWORD'], db="MySQL")
+    conn = DBConnector._dbconnect(MySQL.Connection, ENV["MYSQL_HOST"], ENV["MYSQL_USER"], ENV["MYSQL_PASSWORD"], db="MySQL")
 
     @test typeof(conn) == MySQL.Connection
     @test isopen(conn)
@@ -29,7 +25,7 @@ end
 
 @testset "_dbconnect function for LibPQ" begin
 
-    conn= _dbconnect(LibPQ.Connection,  ENV['POSTGRES_HOST'], ENV['POSTGRES_USER'], ENV['POSTGRES_PASSWORD'], db = "mimic")
+    conn= DBConnector._dbconnect(LibPQ.Connection,  ENV["POSTGRES_HOST"], ENV["POSTGRES_USER"], ENV["POSTGRES_PASSWORD"], db = "mimic")
     @test @isdefined conn
 
 end
