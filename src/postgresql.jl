2ba693d96ad9ecab6a00f28e3d554e76a3f3b7ce
TheCedarPrince <jacobszelko@gmail.com>
2023-07-21 17:29:25 -0400

    Redid postgresql interface

--- src/postgresql.jl
+++ src/postgresql.jl
@@ -4,8 +4,7 @@ Dispatch for LibPQ interface to DBInterface `connect` function; not supported in
 DBInterface.connect(::Type{LibPQ.Connection}, args...; kws...) =
     LibPQ.Connection(args...; kws...)
 
-function _dbconnect(conn_obj::Type{LibPQ.Connection}, host::String, user::String, password::String; db::String="", port::Integer=5432, unix_socket::Union{Nothing,String}=nothing, client_flag=API.CLIENT_MULTI_STATEMENTS, opts = Dict())
-
+function _dbconnect(conn_obj::Type{LibPQ.Connection}; kwargs...)
     conn_string = ""
     for k in kwargs 
         conn_string = conn_string * "$(string(k.first))=$(k.second) "
@@ -16,6 +15,36 @@ function _dbconnect(conn_obj::Type{LibPQ.Connection}, host::String, user::String
     return DBInterface.connect(conn_obj, conn_string)
 
 end
+
+#= 
+
+function _dbconnect(conn_obj::Type{LibPQ.Connection}; host::String, user::String, password::String; db::String="", port::Integer=5432, unix_socket::Union{Nothing,String}=nothing, client_flag=API.CLIENT_MULTI_STATEMENTS, opts = Dict())
+
+    """
+    BUG DESCRIPTION:
+
+    As of now, this function does not work. the conn_string is not built correctly and you'd probably need to build it manually with string interpolations. 
+
+    What this function would do is construct a connection string that looks like:
+
+    "host = /var/run/postgresql user = user password = password dbname = mimiciii"
+
+    However, the problem with the postgresql connection string is that the keywords in the string are actually positionally dependent. You can't just put them in any order.
+    """
+
+
+    conn_string = ""
+    for k in kws
+        conn_string = conn_string * "$(string(k.first))=$(k.second) "
+    end
+
+    conn_string = strip(conn_string)
+
+    return DBInterface.connect(conn_obj, conn_string)
+
+end
+
+=#
 
 """
 Workaround for LibPQ interface to DBInterface's `prepare` function; not supported in LibPQ.jl package currently
